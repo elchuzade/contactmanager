0bc1ea4236ab8fee3975220f9823fde2edc3e908
Kamran Elchuzade <elchuzade@gmail.com>
2019-03-10 21:27:00 +0300

    added functionality to save the changes in the dom on update contact

--- src/context.js
+++ src/context.js
@@ -5,6 +5,15 @@ const Context = React.createContext();
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'UPDATE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.map(contact =>
+          contact.id === action.payload.id
+            ? (contact = action.payload)
+            : contact
+        )
+      };
     case 'DELETE_CONTACT':
       return {
         ...state,
@@ -29,8 +38,7 @@ export class Provider extends Component {
   };
 
   async componentDidMount() {
-    const res = await axios
-      .get('https://jsonplaceholder.typicode.com/users');
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
 
     this.setState({ contacts: res.data });
   }
