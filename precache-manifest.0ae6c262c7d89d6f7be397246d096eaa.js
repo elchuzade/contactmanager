127fbecd39396029e180a9dd4303013af566bd07
Kamran Elchuzade <elchuzade@gmail.com>
2019-03-10 21:40:32 +0300

    Updates

---
+++ precache-manifest.0ae6c262c7d89d6f7be397246d096eaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.4bdee9f6.js"
+  },
+  {
+    "revision": "53ae05cedb055474ef1d",
+    "url": "/contactmanager/static/js/main.aa9bd123.chunk.js"
+  },
+  {
+    "revision": "f54bef1702297fae8017",
+    "url": "/contactmanager/static/js/2.05fda6aa.chunk.js"
+  },
+  {
+    "revision": "53ae05cedb055474ef1d",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "f54bef1702297fae8017",
+    "url": "/contactmanager/static/css/2.2b40eaed.chunk.css"
+  },
+  {
+    "revision": "c932e06bb41315684f7bf661c2a828bd",
+    "url": "/contactmanager/index.html"
+  }
+];
